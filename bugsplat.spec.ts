3dec20caac5555c61ee78d1a9dca3e60855280a0
Bobby Galli <bobbyg603@gmail.com>
2020-12-27 22:52:46 -0500

    Remove formData from the public interface. Autoformatted using the deno formatter.

--- bugsplat.spec.ts
+++ bugsplat.spec.ts
@@ -1,55 +1,63 @@
-import { BugSplat } from './bugsplat.ts';
-import { TestSuite, test } from "https://deno.land/x/test_suite@v0.6.3/mod.ts";
-import { stub, Stub } from "https://deno.land/x/mock@v0.9.4/mod.ts";
+import { BugSplat } from "./bugsplat.ts";
+import { test, TestSuite } from "https://deno.land/x/test_suite@v0.6.3/mod.ts";
+import { Stub, stub } from "https://deno.land/x/mock@v0.9.4/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.80.0/testing/asserts.ts";
 
 interface BugSplatSuiteContext {
-    bugsplat: BugSplat;
-    fetch: Stub<any>;
-    formData: any;
-    formDataValues: any;
+  bugsplat: BugSplat;
+  fetch: Stub<any>;
 }
 
 const database = "fred";
 const appName = "my-deno-crasher-test";
 const appVersion = "1.0";
-const fakeJson = async () => 'success!';
+const fakeJson = async () => "success!";
+let fakeFormData: any;
+let fakeFormDataValues: Array<any>;
+let fetchStub: Stub<any>;
+let formDataStub: Stub<any>;
 
 const bugSplatSuite: TestSuite<BugSplatSuiteContext> = new TestSuite({
-    name: 'BugSplat',
-    beforeEach(context: BugSplatSuiteContext) {
-        context.fetch = stub(globalThis, 'fetch', [{
-            status: 200,
-            json: fakeJson
-        }]);
-        context.bugsplat = new BugSplat(database, appName, appVersion);
-        context.formDataValues = [];
-        context.formData = <any>{
-            append: (key: any, value: any) => {
-                context.formDataValues[key] = value;
-            }
-        };
-        context.bugsplat.formData = () => context.formData;
-    }
+  name: "BugSplat",
+  beforeEach(context: BugSplatSuiteContext) {
+    context.bugsplat = new BugSplat(database, appName, appVersion);
+
+    fakeFormDataValues = [];
+    fakeFormData = <any> {
+      append: (key: any, value: any) => {
+        fakeFormDataValues[key] = value;
+      },
+    };
+
+    formDataStub = stub(context.bugsplat, "_formData", [fakeFormData]);
+    fetchStub = stub(globalThis, "fetch", [{
+      status: 200,
+      json: fakeJson,
+    }]);
+  },
 });
 
-test(bugSplatSuite, 'post should call fetch with url, method and body', (context: BugSplatSuiteContext) => {
+test(
+  bugSplatSuite,
+  "post should call fetch with url, method and body",
+  (context: BugSplatSuiteContext) => {
     const expectedUrl = `https://${database}.bugsplat.com/post/js/`;
-    const error = new Error('BugSplat rocks');
-    
+    const error = new Error("BugSplat rocks");
+
     context.bugsplat.post(error);
 
-    assertEquals(context.fetch.calls, [{
-        args: [
-            expectedUrl,
-            {
-                body: context.formData,
-                method: 'POST'
-            }
-        ],
-        returned: {
-            status: 200,
-            json: fakeJson
-        }
+    assertEquals(fetchStub.calls, [{
+      args: [
+        expectedUrl,
+        {
+          body: fakeFormData,
+          method: "POST",
+        },
+      ],
+      returned: {
+        status: 200,
+        json: fakeJson,
+      },
     }]);
-});
+  },
+);
